f25914644481ab467ad6052ed3eab193705f4f9a
yashakib-dev <yashakib.dev@gmail.com>
2026-04-13 21:34:33 +0600

    Setup Done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,12 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
 import "./globals.css";
+ 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const geist = Geist({
+  variable: "--font-geist",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -19,8 +16,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      lang="en" data-theme="light"
+      className={`${geist.className}  h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
